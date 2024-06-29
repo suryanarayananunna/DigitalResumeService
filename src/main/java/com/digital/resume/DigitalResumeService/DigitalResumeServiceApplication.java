0afdb248f228a3a672325705628870ecd903bf35
suryanarayananunna0612@gmail.com <suryanarayananunna0612@gmail.com>
2024-06-30 01:37:12 +0530

    Initial Commit

---
+++ src/main/java/com/digital/resume/DigitalResumeService/DigitalResumeServiceApplication.java
@@ -0,0 +1,13 @@
+package com.digital.resume.DigitalResumeService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalResumeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalResumeServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digital/resume/DigitalResumeService/DigitalResumeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digital.resume.DigitalResumeService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalResumeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
